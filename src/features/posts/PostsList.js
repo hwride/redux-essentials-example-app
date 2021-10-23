2333ba619173236cff835729b24e9af5794ab939
Hugo Wride <hugowride@gmail.com>
2021-10-23 20:06:00 +0100

    Add time ago post was created.

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { TimeAgo } from './TimeAgo'
+import { PostAuthor } from './PostAuthor'
 
 export const PostsList = () => {
   const posts = useSelector(state => state.posts)
@@ -8,6 +10,8 @@ export const PostsList = () => {
   const renderedPosts = posts.map(post => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
+      <PostAuthor userId={post.user} />
+      <TimeAgo timestamp={post.date} />
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
