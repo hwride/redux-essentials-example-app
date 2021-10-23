be07b9ed9e9842c5713bbd30cccb667b27789b88
Hugo Wride <hugowride@gmail.com>
2021-10-23 19:12:57 +0100

    Add ability to edit post.

---
+++ src/features/posts/EditPostForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+import { postUpdated } from './postsSlice'
+
+export const EditPostForm = ({ match }) => {
+  const { postId } = match.params
+
+  const post = useSelector(state => state.posts.find(post => post.id === postId))
+
+  const [title, setTitle] = useState(post.title)
+  const [content, setContent] = useState(post.content)
+
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  return (
+    <section>
+      <h2>Edit Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          placeholder="What's on your mind?"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={e => setContent(e.target.value)}
+        />
+      </form>
+      <button type="button" onClick={() => {
+        if (title && content) {
+          dispatch(postUpdated({ id: postId, title, content }))
+          history.push(`/posts/${postId}`)
+        }
+      }}>
+        Save Post
+      </button>
+    </section>
+  )
+}
