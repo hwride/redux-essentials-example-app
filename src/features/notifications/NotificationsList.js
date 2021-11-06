e7ea82d08cd49157b4e99908aa664e0ec9fffe24
Hugo Wride <hugowride@gmail.com>
2021-11-06 17:13:58 +0000

    Style new notifications differently.

--- src/features/notifications/NotificationsList.js
+++ src/features/notifications/NotificationsList.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useLayoutEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { formatDistanceToNow, parseISO } from 'date-fns'
 
 import { selectAllUsers } from '../users/usersSlice'
 
-import { selectAllNotifications } from './notificationsSlice'
+import { allNotificationsRead, selectAllNotifications } from './notificationsSlice'
+import classnames from 'classnames'
 
 export const NotificationsList = () => {
+  const dispatch = useDispatch()
   const notifications = useSelector(selectAllNotifications)
   const users = useSelector(selectAllUsers)
+
+  useLayoutEffect(() => {
+    dispatch(allNotificationsRead())
+  })
 
   const renderedNotifications = notifications.map(notification => {
     const date = parseISO(notification.date)
@@ -17,8 +23,12 @@ export const NotificationsList = () => {
       name: 'Unknown User'
     }
 
+    const notificationClassname = classnames('notification', {
+      new: notification.isNew
+    })
+
     return (
-      <div key={notification.id} className="notification">
+      <div key={notification.id} className={notificationClassname}>
         <div>
           <b>{user.name}</b> {notification.message}
         </div>
