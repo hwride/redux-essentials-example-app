5ced55723b9a3685dcb5fec84bcad75eab8d3efc
Hugo Wride <hugowride@gmail.com>
2021-11-28 13:19:47 +0000

    Fix user page to read API slice posts.

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { selectUserById } from './usersSlice'
-import { selectPostsByUser } from '../posts/postsSlice'
+import { createSelector } from '@reduxjs/toolkit'
+import { useGetPostsQuery } from '../api/apiSlice'
 
 export const UserPage = ({ match }) => {
   const { userId } = match.params
 
   const user = useSelector(state => selectUserById(state, userId))
 
-  const postsForUser = useSelector(state => selectPostsByUser(state, userId))
+  // Return a unique selector instance for this page instance so that the filtered results are correctly memoized. This
+  // is required because reselect selectors have their own cache, which by default only has a size of 1.
+  const selectPostsForUser = useMemo(() => createSelector(
+    res => res.data,
+    (res, userId) => userId,
+    (data, userId) => data.filter(post => post.user === userId)
+  ), [])
+
+  const postsForUser = useGetPostsQuery(undefined, {
+    selectFromResult: result => ({
+      // We can optionally include the other metadata fields from the result here
+      ...result,
+      // Include a field called `postsForUser` in the hook result object,
+      // which will be a filtered list of posts
+      postsForUser: selectPostsForUser(result, userId)
+    })
+  }).postsForUser
 
   const postTitles = postsForUser.map(post => (
     <li key={post.id}>
